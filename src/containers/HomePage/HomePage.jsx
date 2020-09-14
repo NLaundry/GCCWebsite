70b012c173f1aacbb93be788d386e1ee4093d2d3
Nathan <nathan@pop-os.localdomain>
2020-09-14 02:00:51 -0400

    Test for Firebase functionality

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,9 +1,65 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import Particles from 'react-particles-js';
 import ParticlesParams from './particles.json'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+import firebase from 'firebase'
 
 function HomePage() {
+
+    const provider = new firebase.auth.GithubAuthProvider();
+    // Configure FirebaseUI.
+    const uiConfig = {
+        // Popup signin flow rather than redirect flow.
+        signInFlow: 'popup',
+        // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+        signInSuccessUrl: '/signedIn',
+        // We will display Google and Facebook as auth providers.
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            firebase.auth.GithubAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ]
+    };
+
+    const login = () => {
+
+        // firebase.auth().signInWithRedirect(provider);
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+            // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+    }
+
+    const db = firebase.firestore()
+
+    const readTest = () => {
+        db.collection("users").add({
+            first: "Ada",
+            last: "Lovelace",
+            born: 1815
+        })
+            .then(function (docRef) {
+                console.log("Document written with ID: ", docRef.id);
+            })
+            .catch(function (error) {
+                console.error("Error adding document: ", error);
+            });
+    }
+
     const background = {
         backgroundColor: "black"
     }
@@ -12,8 +68,10 @@ function HomePage() {
 
     return (
         <div className="HomePage" style={background}>
+            <button onClick={readTest} />
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
             <Particles
-                params={ particlesParams }>
+                params={particlesParams}>
             </Particles>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase'
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCU91gAhbjcCauwJnWWeEM3KLT_AQ8QZlg",
+    authDomain: "gcc-website-2638e.firebaseapp.com",
+    databaseURL: "https://gcc-website-2638e.firebaseio.com",
+    projectId: "gcc-website-2638e",
+    storageBucket: "gcc-website-2638e.appspot.com",
+    messagingSenderId: "1079146980399",
+    appId: "1:1079146980399:web:69fe9ec56dd61c5ec9d2da",
+    measurementId: "G-L9FNLBLWKD"
+})
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
